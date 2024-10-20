5e244bff18c4207d591b32953ca718b67ae0da82
三浦知明 <tomomiura0916@gmail.com>
2024-10-20 15:15:51 +0900

    HomeViewにBindableでstoreを実装

---
+++ Sources/HomeFeature/HomeView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import DetailFeature
+import ComposableArchitecture
+
+public struct HomeView: View {
+    @Bindable var store: StoreOf<HomeReducer>
+    
+    public init(store: StoreOf<HomeReducer>) {
+        self.store = store
+    }
+
+    public var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    Text("User Name")
+                }
+            }
+        }
+        .searchable(text: $store.query)
+    }
+}
+
+#Preview {
+    HomeView(
+        store: .init(initialState: HomeReducer.State()) {
+            HomeReducer()
+        }
+    )
+}
