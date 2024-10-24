ff0617638a9179be60a4b9a8d06d8b98f4f6d3c6
tomoakiWeb <tomomiura0026@gmail.com>
2024-10-25 00:41:27 +0900

    Kingfisherで画像を表示させる

--- Sources/HomeFeature/UserItemView.swift
+++ Sources/HomeFeature/UserItemView.swift
@@ -1,18 +1,29 @@
 import SwiftUI
 import ComposableArchitecture
+import Kingfisher
 
 struct UserItemView: View {
     @Bindable var store: StoreOf<UserItemReducer>
     
     var body: some View {
-        Text(store.userRepo.name)
-            .padding(16)
+        HStack(spacing: 8) {
+            KFImage(store.userRepo.avatarUrl)
+                .resizable()
+                .frame(width: 32, height: 32)
+                .cornerRadius(8)
+            
+            Text(store.userRepo.name)
+            
+            Spacer()
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
     }
 }
 
 #Preview {
     UserItemView(
-        store: .init(initialState: UserItemReducer.State(userRepo: .init(from: .mock(id: 1, login: "test user")))) {
+        store: .init(initialState: UserItemReducer.State(userRepo: .init(from: .mock(id: 1, login: "tomoakiWeb")))) {
             UserItemReducer()
         }
     )
