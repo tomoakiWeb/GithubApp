0b95d811b47fb3bd96bbf6506a290285ac769ec7
三浦知明 <tomomiura0916@gmail.com>
2024-10-24 21:17:37 +0900

    UserItemViewでユーザー名を表示する

--- Sources/HomeFeature/UserItemView.swift
+++ Sources/HomeFeature/UserItemView.swift
@@ -5,13 +5,13 @@ struct UserItemView: View {
     @Bindable var store: StoreOf<UserItemReducer>
     
     var body: some View {
-        Text("UserItemView")
+        Text(store.userRepo.name)
     }
 }
 
 #Preview {
     UserItemView(
-        store: .init(initialState: UserItemReducer.State()) {
+        store: .init(initialState: UserItemReducer.State(userRepo: .init(from: .mock(id: 1, login: "test user")))) {
             UserItemReducer()
         }
     )
