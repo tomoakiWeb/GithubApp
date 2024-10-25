4c03cfb9f6c04a49d2e28b8b6f5fde10fec77f24
tomoakiWeb <tomomiura0026@gmail.com>
2024-10-25 22:47:45 +0900

    UserDetailの作成

---
+++ Sources/ShareModel/UserDetail.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+public struct UserDetail: Equatable, Sendable {
+    public let id: Int
+    public let name: String
+    public let avatarUrl: URL
+    public let fullName: String?
+    public let followers: Int
+    public let following: Int
+}
+
+public extension UserDetail {
+    init (from response: UserDetailResponse) {
+        self.id = response.id
+        self.name = response.login
+        self.avatarUrl = response.avatarUrl
+        self.fullName = response.name
+        self.followers = response.followers
+        self.following = response.following
+    }
+}
+
