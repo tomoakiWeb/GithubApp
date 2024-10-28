de7ad92799b13ebe579eb0e93f82dfe65ba41525
tomoakiWeb <tomomiura0026@gmail.com>
2024-10-28 11:22:17 +0900

    UtilityTestsの作成

--- Sources/Utility/Int+Extension.swift
+++ Sources/Utility/Int+Extension.swift
@@ -4,13 +4,14 @@ public extension Int {
     var formattedWithSuffix: String {
         switch self {
         case 1_000_000...:
-            let formatted = Double(self) / 1_000_000
+            let formatted = floor(Double(self) / 1_000_000 * 10) / 10
             return String(format: "%.1fm", formatted)
         case 1_000...:
-            let formatted = Double(self) / 1_000
+            let formatted = floor(Double(self) / 1_000 * 10) / 10
             return String(format: "%.1fk", formatted)
         default:
             return String(self)
         }
     }
 }
+
